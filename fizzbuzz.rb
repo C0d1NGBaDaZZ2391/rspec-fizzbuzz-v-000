606250d3475156985838f1a1ba4102d09292f78d
Bertha Muteba <nyc_bm26@aol.com>
2018-06-29 19:48:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -10,7 +10,7 @@ end
 
 fizzbuzz(3)
 fizzbuzz(5)
-fizzbuzz(15)
+fizzbuzz(45)
 fizzbuzz(4)
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
